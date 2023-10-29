7ac3f667aeb495c4fb61170ffa37c3a27790e7b9
JulioEvencio <julioigreja.dev@gmail.com>
2023-10-28 21:48:50 -0300

    added: Project created

---
+++ api-order-management/src/main/java/com/github/julioevencio/apiordermanagement/ApiOrderManagementApplication.java
@@ -0,0 +1,13 @@
+package com.github.julioevencio.apiordermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOrderManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiOrderManagementApplication.class, args);
+	}
+
+}
---
+++ api-order-management/src/test/java/com/github/julioevencio/apiordermanagement/ApiOrderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.julioevencio.apiordermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOrderManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
