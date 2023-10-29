c96b2bb1b8f8aa7bec532b850e7d3ea395798a8f
JulioEvencio <julioigreja.dev@gmail.com>
2023-10-29 10:07:05 -0300

    added: doc for UserResource

--- api-order-management/src/main/java/com/github/julioevencio/apiordermanagement/resources/UserResource.java
+++ api-order-management/src/main/java/com/github/julioevencio/apiordermanagement/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.github.julioevencio.apiordermanagement.resources;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,11 +8,33 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.github.julioevencio.apiordermanagement.entities.User;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping(value = "/users")
+@Tag(name = "Users", description = "Endpoints for users")
 public class UserResource {
 	
-	@GetMapping
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	@Operation(
+			summary = "Return all users",
+			description = "Return all users",
+			tags = {"Users"},
+			responses = {
+					@ApiResponse(
+							responseCode = "200",
+							description = "Return all users",
+							content = @Content(
+									mediaType = MediaType.APPLICATION_JSON_VALUE,
+									schema = @Schema(implementation = User.class)
+							)
+					)
+			}
+	)
 	public ResponseEntity<User> findAll() {
 		User u = new User(1L, "Fulano", "fulano@gmail.com", "99999", "12345");
 		
